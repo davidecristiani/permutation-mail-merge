056648fc06271802e982781b30561b2d05ae4846
davidecristiani <14573851+davidecristiani@users.noreply.github.com>
2022-01-30 12:25:41 +0700

    Variables and stub of functions
    Set main variables: $base_text, $placeholder, $contacts, $assembled_texts.
    
    Stub of functions:
    - permutation_mail_merge($base_text, $placeholder, $contacts)
    - print_assembled_texts( $base_text, $placeholder, $contacts, $assembled_texts )

--- src/index.php
+++ src/index.php
@@ -1,3 +1,31 @@
 <?php
 
-echo "Hello world!";
+$base_text = implode( '\n', [
+	"Dear manager,",
+	"This a possible combination of the new work team.",
+	"The team leader will be [contact_placeholder].",
+	"Engineering will be carried out by [contact_placeholder].",
+	"The product manager is a role for [contact_placeholder].",
+	"The online marketer will be [contact_placeholder].",
+	"Our account manager that will keep in touch with customer will be [contact_placeholder].",
+] );
+
+$placeholder = "[contact_placeholder]";
+
+$contacts = [ "Yu", "John", "Tony", "Maria", "Rakshith" ];
+
+$assembled_texts = permutation_mail_merge( $base_text, $placeholder, $contacts );
+
+function permutation_mail_merge( $base_text, $placeholder, $contacts ) {
+
+	// TODO This function will recursively call itself
+	return [];
+
+}
+
+function print_assembled_texts( $base_text, $placeholder, $contacts, $assembled_texts ) {
+
+	// TODO This function will print the input parameters as a proper html page
+	print '';
+
+}
